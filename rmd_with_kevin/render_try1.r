f6283635f5ccf76a005678a1d39e2e60bfe3a139
WATTBA <nugentc@uoguelph.ca>
2018-02-09 12:49:36 -0500

    quick run through of red with kevin

---
+++ rmd_with_kevin/render_try1.r
@@ -0,0 +1,7 @@
+rmarkdown::render('cams_first_try.Rmd')
+
+
+rmarkdown::render('cams_first_try.Rmd', output_file='doc')
+
+rmarkdown::render('cams_first_try.Rmd', "pdf_document")
+rmarkdown::render('cams_first_try.Rmd', "html_document")

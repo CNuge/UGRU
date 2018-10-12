135a6bd0874a70af03dc808b7f796f289aab37b5
CNuge <nugentc@uoguelph.ca>
2018-10-12 12:13:16 -0400

    i made it work but its slower :-1:

--- bad_code_clean/Code_to_convert_to_tidyverse.r
+++ bad_code_clean/Code_to_convert_to_tidyverse.r
@@ -11,10 +11,46 @@ head(dat)
 dat$Window_Size = dat$End - dat$Start
 #but its not needed?
 
-#no its a lil window thingy
-Window.Size = 150000
+#no its a lil fixed window thingy
+Window.Size = 100000
 
-comm_by_chromosome <- lapply(split(dat, dat$Chromosome), function(y) t(sapply(split(y, list(cut(y[,"End"], as.integer(max(y$End)/as.integer(Window.Size))))), function(x) table(x$Family))))
+comm_by_chromosome = lapply(split(dat, dat$Chromosome), 
+						function(y) t(sapply(split(y, list(cut(y[,"End"], as.integer(max(y$End)/as.integer(Window.Size))))), 
+							function(x) table(x$Family))))
+
+head(comm_by_chromosome[[1]])
+
+
+
+chr_counts = function(df){
+	fam_count_chr = data.frame(Family = factor(unique(df$Family)))
+	fam_count_chr
+	
+	windows = seq(100000, max(df$End), 100000)
+	
+	for(seg in windows){
+		fam_count_chr[,paste(seg)] = 0 
+
+		subset_a = df[df$End < seg ,]
+
+		subset = subset_a[df$End > (seg-100000), ]
+		for(trans in fam_count_chr$Family){
+
+			count = length(subset$Family[subset$Family == trans])
+			fam_count_chr[fam_count_chr$Family == trans, paste(seg)]	= count
+
+		}
+	}
+
+	return(fam_count_chr)
+}
+	
+
+list_of_df = split(dat, dat$Chromosome)
+
+chr_counts_df = lapply(list_of_df, chr_counts)
+
+chr_counts_df
 
 #########
 #########
@@ -30,22 +66,24 @@ all_fams = factor(unique(dat$Family))
 length(all_fams)
 levels(all_fams)
 
-comm_by_chromosome = data.frame(Family = all_fams)
-comm_by_chromosome
+fam_count_chr = data.frame(Family = all_fams)
+fam_count_chr
 
 for(chr in all_chr){
-	comm_by_chromosome[,chr] = 0 
+	fam_count_chr[,chr] = 0 
 
 	subset = dat[dat$Chromosome == chr ,]
 
-	for(trans in comm_by_chromosome$Family){
-		count = length(subset$Family[subset$Family == trans])
-		comm_by_chromosome[comm_by_chromosome$Family == trans, chr]	= count
-	}
+	for(trans in fam_count_chr$Family){
 
+		count = length(subset$Family[subset$Family == trans])
+		fam_count_chr[fam_count_chr$Family == trans, chr]	= count
+
+	}
 }
 
-head(comm_by_chromosome) #dataframe with chr as columns and family counts in rows
+head(fam_count_chr) #dataframe with chr as columns and family counts in rows
 
 
 #challenge - one line the above with a groupby
+

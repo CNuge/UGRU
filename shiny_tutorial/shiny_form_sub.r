3ec090aef4a90d0120b1fe25997a84b532bea8d8
CNuge <nugentc@uoguelph.ca>
2018-12-17 10:43:41 -0500

    shiny notes

--- shiny_tutorial/shiny_form_sub.r
+++ shiny_tutorial/shiny_form_sub.r
@@ -102,6 +102,20 @@ server = function(input, output){
 }
 
 
+server = function(input, output, session) {
+    observe({
+      mandatoryFilled <-
+        vapply(fieldsMandatory,
+               function(x) {
+                 !is.null(input[[x]]) && input[[x]] != ""
+               },
+               logical(1))
+      mandatoryFilled <- all(mandatoryFilled)
+      
+      shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+    })    
+  }
+
 shinyApp(ui = ui, server = server)
 
 

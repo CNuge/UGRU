59bd99c80eeff5837abb95d80593b0d763b0901e
CNuge <nugentc@uoguelph.ca>
2019-01-11 11:44:26 -0500

    matts tutorial for the new semester

---
+++ W19_Week1_AvocadoPrice-master/Avocado.R
@@ -0,0 +1,84 @@
+## Into the Tidyverse: Data manipulation and plotting with dplyr and ggplot2
+## Script by Matt Brachmann
+## We are using a free data set from Kaggle to analyze 
+## The prices of Avocados in the USA
+
+install.packages('patchwork')
+
+library(tidyverse)  	## Best package ever
+library(wesanderson) 	## Makes plots coloured as Wes Anderson movie themes
+library(patchwork) 		## Allows you to make really nice plots
+library(janitor) 	 	## Makes coloumn names a uniform format
+
+data = read_csv('avocado.csv') %>%  ## Read in the data. This data was provided by Kaggle
+  select(-X1) %>%   ## Remove this coloumn as it repeats the index
+  clean_names('screaming_snake') %>%  ## Makes all of the coloumn names a uniform format
+  ## Column names are now caps locked and aggressive 
+  separate(col = DATE, into = c('YEAR', 'MONTH', 'DAY'),  ## The date is all together and we don't want that
+  ## we want the dates separated into year, month, and day 
+           sep = '-', remove = FALSE) %>%  ## the sep argument tells the function what to split the data on
+  select(-DATE, -X4046, -X4225, -X4770) %>% ## Get rid of the trash columns
+  filter(REGION == 'TotalUS') %>%  ## We only want the Avocado prices for the total USA not state by state
+  mutate(MONTHNAME = as.factor(case_when(  ## mutate lets you create a column
+    MONTH == '01' ~ 'January',    			## we want to convert the month from a number to an actual word
+    MONTH == '02' ~ 'Febuary',				## case_when is the best thing in the world, Joey showed it to me
+    MONTH == '03' ~ 'March',
+    MONTH == '04' ~ 'April',
+    MONTH == '05' ~ 'May',
+    MONTH == '06' ~ 'June',
+    MONTH == '07' ~ 'July',
+    MONTH == '08' ~ 'August',
+    MONTH == '09' ~ 'September',
+    MONTH == '10' ~ 'October',
+    MONTH == '11' ~ 'November',
+    MONTH == '12' ~ 'December'))) %>% 
+  rename(NUMSMALLBAGS = SMALL_BAGS,  ## Rename these coloumns from trash names to actual names
+         NUMLARGEBAGS = LARGE_BAGS,
+         NUMEXTRALGBAGS = X_LARGE_BAGS)
+
+theme_set(theme_bw())  ## Sets your plotting theme ahead of time. This just saves some time. 
+## SET YOUR THEME TO WHATEVER YOU WANT!
+
+## BIG PLOTTING SIDE NOTE!
+## specify aes() anytime you want to link the plot to your data. 
+## in the code below, we use aes() to refer to what the x and y variables are and the point colors
+## We do not specify anything for the lines however, we could plot individual lines for each avocado type
+## We can do this: geom_smooth(method = 'lm', aes(col = TYPE))
+
+
+## Plotting a normal scatter plot with a line
+p1 = ggplot(data = data, aes(x = TOTAL_VOLUME, y = AVERAGE_PRICE))+  ## First step in becoming a ggplot2 master, set the data up. 
+  geom_point(aes(col = TYPE))+ ## tells ggplot that you're plotting actual points... this makes the dots appear
+  geom_smooth(method = 'lm', col = 'black')+ ## This makes the lines appear, lets us plot a linear relationship
+  labs(x = 'Total Volume', y = 'Average price of avocados')+ ## the labs function changes the x and y labels
+  scale_color_manual(values = wes_palette('Darjeeling1', n = 5,  ## This lets us scale the colours to whatever we want
+                                          type = 'continuous'))+ ## and of course we want plots themed as Wes Anderson movies
+  theme(panel.grid.major = element_blank(), ## Get the trash gridlines out of here, gets rid of major grid lines
+        panel.grid.minor = element_blank(), ## Gets rid of the minor grid lines
+        axis.text = element_text(size = 6)) ## Increases the text size
+
+
+## Making fancy violin plots
+## These are the best plots ever and should be used constantly (my opinion)
+p2 = ggplot(data = data, aes(x = MONTHNAME, y = AVERAGE_PRICE))+ ## Set this up the same way as in the plot above
+  geom_violin(aes(col = TYPE, fill = TYPE))+ ## Specifying that we want a violin plot
+  ## note that to make the colors work we need to speify both col and fill
+  theme(axis.text.x = ## we need to make the x axis text not terrible
+          element_text(size  = 6, ## changes size
+                       angle = 45, ## rotates it on a 45 degree angle
+                       hjust = 1, ## adjusts it horizontally
+                       vjust = 1), ## adjusts it vertically
+        axis.text.y = element_text(size = 6)) + ## change the size of the y axis text to match the x axis
+  labs(x = "Month", y = "Average price ('Merica Dollars)")+ ## Axis labels
+  scale_color_manual(values = wes_palette('Darjeeling1', n = 5,  ## Specifies which Wes Anderson palette to use
+                                          type = 'continuous'))+
+  scale_fill_manual(values = wes_palette('Darjeeling1', n = 5,  ## Remember to always specify the fill
+  	## This fills the plot with color
+                                         type = 'continuous'))+
+  theme(panel.grid.major = element_blank(),  ## Get rid of major panel grid lines
+        panel.grid.minor = element_blank())  ## Get rid of the minor panel grid lines
+  
+p1/p2  ## This syntax is a part of the patchwork function
+## patchwork allows us to link plots and plot them all in the same window
+## I personally find this the best way to format plots into a single picture
+## Joey showed me this package
---
+++ W19_Week1_AvocadoPrice-master/README.md
@@ -0,0 +1,2 @@
+# W19_Week1_AvocadoPrice
+This code is for a quick review and refresher on dplyr and ggplot2. This code goes over some of the major functions that people use to manipulate data. This code also makes some nice plots and highlights some interesting packages

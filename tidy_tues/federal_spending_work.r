20f645a87d00bccfda3f5be5e900a480df774b61
CNuge <nugentc@uoguelph.ca>
2019-02-14 16:26:43 -0500

    fed spending code

---
+++ tidy_tues/federal_spending_work.r
@@ -0,0 +1,14 @@
+library(tidyverse)
+library(reshape2)
+library(RCurl)
+library(ggthemes)
+
+
+federal_git = getURL('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-12/energy_spending.csv')
+fed_df = read_csv(federal_git)
+
+names(fed_df)
+
+fed_df$department[237] == "High-Energy Physics*"
+fed_df$year
+fed_df$energy_spending
